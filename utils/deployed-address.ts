3777640680c8230b30042d72d7e45bc38fec7bfe
alp80 <alp.demir0404@gmail.com>
2024-08-02 10:24:42 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xB69C321DCb9749F1E0c2B554700a80942DB8082a'
+export const ProxyAdmin = '0x4017DA9E211b69dD15aB48632B3c5354feAE7F31'
+export const SWTRImplementation = '0x3f10590A63B2bf13c996661b1936c14CFF5ECD28'
     
